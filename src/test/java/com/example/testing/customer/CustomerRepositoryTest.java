3565ab125a05c31c08bb2a2eea50d928c7319691
anthonyallen <allen.1295@osu.edu>
2021-04-26 22:34:35 -0400

    Testing Customer SQL and JPA query

---
+++ src/test/java/com/example/testing/customer/CustomerRepositoryTest.java
@@ -0,0 +1,42 @@
+package com.example.testing.customer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.util.Optional;
+import java.util.UUID;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+@DataJpaTest
+class CustomerRepositoryTest {
+
+    @Autowired
+    private CustomerRepository underTest;
+
+    @Test
+    void itShouldSelectCustomerByPhoneNumber() {
+        //Given
+        //When
+        //Then
+    }
+
+    @Test
+    void itShouldSaveCustomer() {
+        //Given
+        UUID id = UUID.randomUUID();
+        Customer customer = new Customer(id,"Anthony", "0000000000");
+        //When
+        underTest.save(customer);
+        //Then
+        Optional<Customer> optionalCustomer = underTest.findById(id);
+        assertThat(optionalCustomer).isPresent().hasValueSatisfying(c -> {
+            assertThat(c.getId()).isEqualTo(id);
+            assertThat(c.getName()).isEqualTo("Anthony");
+            assertThat(c.getPhoneNumber()).isEqualTo("0000000000");
+//            assertThat(c).isEqualToComparingFieldByField(customer);
+        });
+    }
+}
