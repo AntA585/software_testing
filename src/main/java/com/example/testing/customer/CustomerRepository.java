ac7997ff87725fdc9ae0dc6d0378a4719f18b6b6
anthonyallen <allen.1295@osu.edu>
2021-05-12 16:37:42 -0400

    Implemented testing for the CustomerRepository, testing the query that was made. Also added a property in the property file that keeps the table columns as they are in the Customer object

--- src/main/java/com/example/testing/customer/CustomerRepository.java
+++ src/main/java/com/example/testing/customer/CustomerRepository.java
@@ -9,7 +9,12 @@ import java.util.UUID;
 
 public interface CustomerRepository extends CrudRepository<Customer, UUID> {
 
-    @Query(value = "select id, name,phoneNumber from customer where phoneNumber = :phoneNumber",nativeQuery = true)
+    //@Query(value = "select id, name, phoneNumber from Customer where phoneNumber = :phoneNumber",nativeQuery = true)
+    @Query(
+            value = "select id, name, phoneNumber " +
+                    "from customer where phoneNumber = phoneNumber",
+            nativeQuery = true
+    )
     Optional<Customer> selectCustomerByPhoneNumber(@Param("phoneNumber") String phoneNumber);
 
 }
